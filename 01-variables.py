d17f8701990d3de58378ec4deb8109d707706d1f
“Pizzapasta2005” <leonardopena808@gmail.com>
2026-05-12 16:39:53 -0400

    commit "lol"

--- 01-variables.py
+++ 01-variables.py
@@ -1,5 +1,15 @@
+#Definicion de variables
 nombre = "Leonardo"
 apellido = "Peña"
 edad = 20 
 
 print ( "mi nombre es",nombre , "y mi apellido es", apellido, "y mi edad es", edad)
+
+#se utiliza el # para hacer comentarios sin afectar o alterar el codigo
+
+"""
+y esto es un comentario multilinea por que se sigue hacia abajo siempre 
+y cuando sigan habiendo tres comillas al extremo del texto
+
+"""
+
